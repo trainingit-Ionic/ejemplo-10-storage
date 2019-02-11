da1f2e739e551d3fd29cbfe34fa1f7bd29653930
Carlos Herrera Conejero <carherco@gmail.com>
2019-02-11 17:33:42 +0100

    código del ejemplo visto en el vídeo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  driver: string = '';
+  name: string = '';
+
+  constructor(private storage: Storage) {
+    this.storage.ready().then(
+      () => this.driver = this.storage.driver
+    );
+  }
+
+  save() {
+    this.storage.set('name',this.name);
+  }
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  name: string = '';
+
+  constructor(private storage: Storage) {
+    
+  }
+
+  ionViewWillEnter() {
+    this.storage.ready().then(
+      () => this.storage.get('name').then(
+        (value) => this.name = value
+      )
+    );
+  }
+}
